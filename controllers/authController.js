ee5fa1bf96fdace664eb814781c4ff885d9f1d52
Ateeb054 <muhammadateeb054@gmail.com>
2026-09-19 18:54:42 +0500

    Refresh Token verify and gen new access token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import userModel from "../models/userModels.js"
+import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
 import { accessToken,refreshToken } from "../services/tokenServices.js"
 
@@ -44,6 +45,21 @@ if(match){
     }
     catch(error){
         res.status(400).json({msg:"server error"})
+    }
+}
+
+export const refresh = async (req, res) => {
+    try {
+const ref=req.cookies.refreshToken
+
+const match=jwt.verify(ref,process.env.REFRESH_SECRET)
+if(!match){
+    return res.status(400).json({msg:"invalid refresh token"})
+}
+const acc = jwt.sign({id:match.id,email:match.email},process.env.ACCESS_SECRET,{expiresIn:"15m"})
+res.status(200).json({msg:"access token generated",accessToken:acc})
+    } catch (error) {
+res.status(400).json({msg:"cookie error",e:error.message})
     }
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { login, logout, signup } from "../controllers/authController.js";
+import { login, logout, signup ,refresh } from "../controllers/authController.js";
 
 const authRoutes = Router()
 
@@ -8,6 +8,8 @@ authRoutes.post("/login" ,login)
 authRoutes.post("/signup" , signup)
 
 authRoutes.post("/logout", logout)
+
+authRoutes.post("/refresh", refresh)
 
 
 export default authRoutes
